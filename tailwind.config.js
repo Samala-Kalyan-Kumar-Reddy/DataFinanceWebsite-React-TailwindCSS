ca41a24bf24332ea6f2f3ee974e0d3ea8b1d338d
Samala Kalyan Kumar Reddy <82137130+Samala-Kalyan-Kumar-Reddy@users.noreply.github.com>
2024-07-07 12:54:59 -0500

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Ensure you include the correct paths
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
